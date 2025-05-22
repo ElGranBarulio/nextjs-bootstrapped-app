a725b1f482fc68e330c1bd258cb0cdbeb35a568a
Braulio <zornozamoralesbraulio225@gmail.com>
2025-05-22 11:01:55 +0200

    Add .next/static/chunks/src_app_groups_[id]_page_tsx_a993527f._.js 1747904515105

---
+++ .next/static/chunks/src_app_groups_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_028d6ccf._.js",
+  "static/chunks/src_70a82315._.js"
+],
+    source: "dynamic"
+});
